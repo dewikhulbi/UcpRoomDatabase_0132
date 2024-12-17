caf3a3784060ea1d711bb50959730180c89f6883
dewikhulbi <dewikhlb20@gmail.com>
2024-12-17 22:04:47 +0700

    edit nama

--- app/src/main/java/com/example/ucp2/data/entity/Matkul.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Matkul.kt
@@ -3,8 +3,8 @@ package com.example.ucp2.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "matakuliah")
-data class MataKuliah(
+@Entity(tableName = "matkul")
+data class Matkul(
     @PrimaryKey
     val kode: String,
     val nama: String,
