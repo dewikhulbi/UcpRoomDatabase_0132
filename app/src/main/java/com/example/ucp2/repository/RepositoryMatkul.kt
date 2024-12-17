3f0c68b466a7f907aa002dfa9913d88b502e806e
dewikhulbi <dewikhlb20@gmail.com>
2024-12-17 22:09:27 +0700

    memperbaiki RepositoryMatkul

--- app/src/main/java/com/example/ucp2/repository/RepositoryMatkul.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMatkul.kt
@@ -1,12 +1,12 @@
 package com.example.ucp2.repository
 
-import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.data.entity.Matkul
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMatkul {
-    suspend fun insertMatkul(mataKuliah: MataKuliah)
-    fun getAllMatkul(): Flow<List<MataKuliah>>
-    fun getMatkul (nim: String): Flow<MataKuliah>
-    suspend fun deleteMatkul(mahasiswa: MataKuliah)
-    suspend fun updateMatkul(mahasiswa: MataKuliah)
+    suspend fun insertMatkul(matkul: Matkul)
+    fun getAllMatkul(): Flow<List<Matkul>>
+    fun getMatkul (kode: String): Flow<Matkul>
+    suspend fun deleteMatkul(matkul: Matkul)
+    suspend fun updateMatkul(matkul: Matkul)
 }
