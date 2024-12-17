3a015c5ea3268ec729c269c7d8a146f9b5a1e003
dewikhulbi <dewikhlb20@gmail.com>
2024-12-17 22:00:58 +0700

    membuat database untuk dosen dan mata kuliah untuk menyimpan data

--- app/src/main/java/com/example/ucp2/data/database/DomaDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/DomaDatabase.kt
@@ -5,16 +5,16 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2.data.dao.DosenDao
-import com.example.ucp2.data.dao.MataKuliahDao
+import com.example.ucp2.data.dao.MatkulDao
 import com.example.ucp2.data.entity.Dosen
-import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.data.entity.Matkul
 
-@Database(entities = [Dosen::class, MataKuliah::class], version = 2, exportSchema = false)
+@Database(entities = [Dosen::class, Matkul::class], version = 2, exportSchema = false)
 abstract class DomaDatabase : RoomDatabase() {
 
     // Mendefinisikan fungsi untuk mengakses DAO masing-masing entitas
     abstract fun dosenDao(): DosenDao
-    abstract fun mataKuliahDao(): MataKuliahDao
+    abstract fun mataKuliahDao(): MatkulDao
 
     companion object {
         @Volatile // Memastikan bahwa nilai variabel instance selalu sama di semua thread
