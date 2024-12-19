92cdfe6fd2cdd96375aca93f53d30c94dc440a9a
dewikhulbi <dewikhlb20@gmail.com>
2024-12-20 04:23:55 +0700

    abstract fun untuk mendefinisikan fungsi untuk mengakses Dao masing-masing entitasI

--- app/src/main/java/com/example/ucp2/data/database/DomaDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/DomaDatabase.kt
@@ -9,26 +9,14 @@ import com.example.ucp2.data.dao.MatkulDao
 import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.data.entity.Matkul
 
+
 @Database(entities = [Dosen::class, Matkul::class], version = 2, exportSchema = false)
 abstract class DomaDatabase : RoomDatabase() {
 
     // Mendefinisikan fungsi untuk mengakses DAO masing-masing entitas
     abstract fun dosenDao(): DosenDao
-    abstract fun mataKuliahDao(): MatkulDao
+    //abstract fun mataKuliahDao(): MatkulDao
+    abstract fun matkulDao(): MatkulDao
 
-    companion object {
-        @Volatile // Memastikan bahwa nilai variabel instance selalu sama di semua thread
-        private var Instance: DomaDatabase? = null
-        fun getDatabase(context: Context): DomaDatabase {
-            return (Instance ?: synchronized(this) {
-                Room.databaseBuilder(
-                    context,
-                    DomaDatabase::class.java, // class database
-                    "DomaDatabase" // nama database
-                )
-                    .fallbackToDestructiveMigration() // Menangani perubahan skema jika diperlukan
-                    .build().also { Instance = it }
-            })
-        }
-    }
+
 }
