ae618560eef0ebadcf46281905752191de30a555
dewikhulbi <dewikhlb20@gmail.com>
2024-12-20 02:02:52 +0700

    membuat alamat navigasi untuk dosen dan mahasiswa

---
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.navigation
+
+interface AlamatNavigasiDosen {
+    val route: String
+}
+object DestinasiHomeDosen : AlamatNavigasiDosen {
+    override val route = "home_dosen"
+}
+object DestinasiHomeMenu : AlamatNavigasiDosen {
+    override val route = "home_menu"
+}
+
+object DestinasiHomeMatkul : AlamatNavigasiDosen {
+    override val route = "home_mahasiswa"
+}
+object DestinasiDetailDosen : AlamatNavigasiDosen {
+    override val route = "detail"
+    const val NIDN = "nidn"
+    val routesWithArg = "$route/{$NIDN}"
+}
+object DestinasiDetailMatkul : AlamatNavigasiDosen {
+    override val route = "detail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+object DestinasiUpdateMatkul : AlamatNavigasiDosen {
+    override val route = "update"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
