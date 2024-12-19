bc8067639117f48f7308322e5948084a57dd8e35
dewikhulbi <dewikhlb20@gmail.com>
2024-12-20 02:09:59 +0700

    InsertBodyDosen: Fungsi atau composable yang menangani UI untuk input data dosen,

---
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDosenView.kt
@@ -0,0 +1,91 @@
+package com.example.ucp2.ui.view.dosen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.costumwidget.TopAppBar
+import com.example.ucp2.ui.navigation.AlamatNavigasiDosen
+import com.example.ucp2.ui.viewmodeldosen.DosenEvent
+import com.example.ucp2.ui.viewmodeldosen.DosenUIState
+import com.example.ucp2.ui.viewmodeldosen.DosenViewModel
+import com.example.ucp2.ui.viewmodeldosen.FormErrorState
+import com.example.ucp2.ui.viewmodeldosen.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiInsertDosen : AlamatNavigasiDosen{
+    override val route: String = "insert_dosen"
+}//object dikenal sebagai nama halamnan di insertmhsview
+
+@Composable
+fun InsertDosenView(
+    onBack:() -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState=viewModel.uiState //ambil ui state dari viewmodel
+    val snackbarHostState = remember { SnackbarHostState() } //snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    //observasi perubahan snackbarmassage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) //tampilansnackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ){ padding ->
+        Column (
+            modifier = Modifier.fillMaxSize().padding(padding).padding(16.dp)
+        ){
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Dosen",
+                modifier = Modifier
+
+            )
+            //isi body
+            InsertBodyDosen(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent) //update state di viewmodel
+                },
+                onClick = {
+                    viewModel.saveData() //simpan data
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
