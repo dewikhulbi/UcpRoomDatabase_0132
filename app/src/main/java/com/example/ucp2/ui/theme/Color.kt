8241f1bad9611fbc2536f7344fc9c60681107907
dewikhulbi <dewikhlb20@gmail.com>
2024-12-20 04:19:31 +0700

    mengganti warna tampilan aplikasi

--- app/src/main/java/com/example/ucp2/ui/theme/Color.kt
+++ app/src/main/java/com/example/ucp2/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.ucp2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF108315)
+val PurpleGrey80 = Color(0xFF618C2F)
+val Pink80 = Color(0xFF1F7324)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF3480D2)
+val PurpleGrey40 = Color(0xFF217723)
+val Pink40 = Color(0xFF28802C)
