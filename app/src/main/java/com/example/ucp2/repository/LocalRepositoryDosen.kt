2f799ff6dcc3f4ab743d8dcc8463ddae11a9c68c
dewikhulbi <dewikhlb20@gmail.com>
2024-12-20 04:26:03 +0700

    untuk mengelola data Dosen menggunakan pattern Repository. Repository ini bertujuan untuk memisahkan logika data dari ViewModel atau layer bisnis, sehingga arsitektur aplikasi menjadi lebih modular dan terorganisir.

---
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDosen.kt
@@ -0,0 +1,21 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDosen (
+    private val dosenDao: DosenDao) : RepositoryDosen {
+
+        override suspend fun insertDosen(dosen: Dosen) {
+            dosenDao.insertDosen(dosen)
+        }
+
+        override fun getAllDosen(): Flow<List<Dosen>> {
+            return dosenDao.getAllDosen()
+        }
+
+        override fun getDosen(nidn: String): Flow<Dosen> {
+            return dosenDao.getDosen(nidn)
+        }
+}
